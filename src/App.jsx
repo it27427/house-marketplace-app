3deff01ad597f0d766e300c1e7311d558d7baf71
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:35:42 +0600

    Offers page added in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Explore from '@/pages/Explore';
+import Offers from '@/pages/Offers';
 
 const App = () => {
   return (
