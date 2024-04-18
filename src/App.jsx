d36e91457b876cb6751efb396d3b3e0cb305c622
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:36:04 +0600

    Profile page imported in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Explore from '@/pages/Explore';
 import Offers from '@/pages/Offers';
+import Profile from '@/pages/Profile';
 
 const App = () => {
   return (
