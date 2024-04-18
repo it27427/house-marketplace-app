42748b47c3c399dab99043607c9b58f5b97c7eb4
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 16:57:14 +0600

    vite-config updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-// import svgr from 'vite-plugin-svgr';
 import svgr from '@svgr/rollup';
 
 export default defineConfig({
