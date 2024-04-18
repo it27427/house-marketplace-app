dfa2c32e4ea49cc2234cd6c3ebb5eb9590abf3c1
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:19:34 +0600

    alias-configuration completed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import envCompativle from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      routes: `${path.resolve(__dirname, './src/routes/')}`,
+      services: `${path.resolve(__dirname, './src/services/')}`,
+    },
   },
 });
