d44435eb5fcfd5a747a544a7d06743b9001feb68
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:50:19 +0600

    NotFound page added

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='flex-column-center'>
+      <h2>Page Not Found</h2>
+      <a href='/' className='btn-back'>
+        Back To Home
+      </a>
+    </div>
+  );
+};
+
+export default NotFound;
