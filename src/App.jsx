5ac78ce1dafc835fa50b12e123db0ee8091ce862
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:37:24 +0600

    ForgotPassword imported in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Offers from '@/pages/Offers';
 import Profile from '@/pages/Profile';
 import SignIn from '@/pages/SignIn';
 import SignUp from '@/pages/SignUp';
+import ForgotPassword from '@/pages/ForgotPassword';
 
 const App = () => {
   return (
