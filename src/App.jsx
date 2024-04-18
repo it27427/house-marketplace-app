13e9688673f440ba14ad7ec2ffe01364264cac51
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:21:04 +0600

    page path added by alias and component directory added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Explore from './pages/Explore';
+import Explore from '@/pages/Explore';
 
 const App = () => {
   return (
