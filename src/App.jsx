6a3e3fcf915c2ea440ef54675d21f59ab48c8e98
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:36:33 +0600

    SignIn imported in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Explore from '@/pages/Explore';
 import Offers from '@/pages/Offers';
 import Profile from '@/pages/Profile';
+import SignIn from '@/pages/SignIn';
 
 const App = () => {
   return (
