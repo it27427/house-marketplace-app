e8a6f49670200406948909c8557e81656b192ef2
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:37:09 +0600

    SignUp imported in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Explore from '@/pages/Explore';
 import Offers from '@/pages/Offers';
 import Profile from '@/pages/Profile';
 import SignIn from '@/pages/SignIn';
+import SignUp from '@/pages/SignUp';
 
 const App = () => {
   return (
