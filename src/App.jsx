bf6c925e979a532cc0c630e34aa5e364e84a7140
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-18 15:39:13 +0600

    SignUp route added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path='/offers' element={<Offers />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
       </Routes>
     </Router>
   );
