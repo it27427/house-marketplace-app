b88a84973fb67f78eb3766cf76335cb19535b70a
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-20 15:51:00 +0600

    import icons, link, usenavigate and useState in SignIn Component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/images/svg/keyboardArrowRightIcon.svg';
 
 const SignIn = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h2>Sign In Page</h2>
