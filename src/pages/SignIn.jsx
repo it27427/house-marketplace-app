d1aade233b5218994e250666d98dc0da257dc875
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-21 01:37:13 +0600

    sign-in OnChange

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -13,7 +13,13 @@ const SignIn = () => {
 
   const navigate = useNavigate();
 
-  const onChange = () => {};
+  const onChange = (e) => {
+    e.preventDefault();
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
   return (
     <>
