91b3c880e2b24f8acc1bdc18b7980cb3e6e5b30e
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-20 15:52:35 +0600

    visibilityIcon import

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { ReactComponent as ArrowRightIcon } from '../assets/images/svg/keyboardArrowRightIcon.svg';
+import { ReactComponent as ArrowRightIcon } from '@/assets/images/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '@/assets/images/svg/visibilityIcon.svg';
 
 const SignIn = () => {
   const navigate = useNavigate();
