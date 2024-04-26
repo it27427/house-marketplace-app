a2fe75b92d15ad689811e0930c1ee9436bcf385b
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-26 16:24:08 +0600

    handleGoogleClick

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { db } from '@/firebase.config';
+import { toast } from 'react-toastify';
+import googleIcon from '@/assets/images/svg/googleIcon.svg';
 
 const OAuth = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleGoogleClick = () => {};
+
   return (
-    <div>
-      <p>OAuth</p>
+    <div className='socialLogin'>
+      <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with</p>
+      <button
+        onClick={handleGoogleClick}
+        type='button'
+        className='socialIconDiv'
+      >
+        <img src={googleIcon} alt='google-icon' />
+      </button>
     </div>
   );
 };
