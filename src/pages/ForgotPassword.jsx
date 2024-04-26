2a00fa4cce0575c6561ea34337591b946d69ff3d
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-26 15:54:39 +0600

    forgot-password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { ReactComponent as ArrowRightIcon } from '@/assets/images/svg/keyboardArrowRightIcon.svg';
 
 const ForgotPassword = () => {
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+
   return (
     <div>
       <h2>Forgot Password Page</h2>
