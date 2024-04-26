98138b31d447edd28b0d420738921c6ae5f55bdb
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-26 16:04:13 +0600

    ForgotPassword Markup Completed

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -5,13 +5,43 @@ import { toast } from 'react-toastify';
 import { ReactComponent as ArrowRightIcon } from '@/assets/images/svg/keyboardArrowRightIcon.svg';
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState('');
+
   const handleChange = (e) => {};
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <div>
-      <h2>Forgot Password Page</h2>
+    <div className='pageContainer'>
+      <header className='loginHeader'>
+        <h4 className='pageHeader'>Forgot Password</h4>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='email'
+            placeholder='Enter email address'
+            id='email'
+            className='emailInput'
+            value={email}
+            onChange={handleChange}
+          />
+
+          <Link to='/sign-in' className='forgotPasswordLink'>
+            Sign In
+          </Link>
+
+          <div className='signInBar'>
+            <p className='signInText'>Send Reset Link</p>
+            <button type='submit' className='signInButton'>
+              <ArrowRightIcon fill='#ffffff' width='24px' height='24px' />
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 };
