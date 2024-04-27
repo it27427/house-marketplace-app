5f43dcb1605e746211f69257c76b4a6f07512d7e
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-27 13:31:33 +0600

    ListingItem-Done

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as DeleteIcon } from '@/assets/images/svg/deleteIcon.svg';
+import bedIcon from '../assets/images/svg/bedIcon.svg';
+import bathtubIcon from '../assets/images/svg/bathtubIcon.svg';
 
-const ListingItem = () => {
+const ListingItem = ({ listing, id }) => {
   return (
-    <div>
-      <p>Listing Item</p>
-    </div>
+    <li className='categoryListing'>
+      <Link
+        to={`/category/${listing.type}/${id}`}
+        className='categoryListingLink'
+      >
+        <img
+          src={listing.imgUrls[0]}
+          alt={listing.name}
+          className='categoryListingImg'
+        />
+      </Link>
+    </li>
   );
 };
 
