26f4a30ee22d369a805c42626a331b7ccf2341e1
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-27 12:52:30 +0600

    category-page added in app

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PrivateRoute from '@/components/PrivateRoute';
 // IMPORT-PAGES
 import Explore from '@/pages/Explore';
 import Offers from '@/pages/Offers';
+import Category from '@/pages/Category';
 import Profile from '@/pages/Profile';
 import SignIn from '@/pages/SignIn';
 import SignUp from '@/pages/SignUp';
@@ -25,6 +26,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
